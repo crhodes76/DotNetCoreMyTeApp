0d44387de9a955ae05fa5a9ed1c8c523480bcf96
Charles Rhodes <crhodes76@users.noreply.github.com>
2025-03-24 15:45:14 -0400

    added package EntityFrameworkCore Tools

--- DotNetCoreMyTeApp/obj/Debug/net6.0/DotNetCoreMyTeApp.AssemblyInfo.cs
+++ DotNetCoreMyTeApp/obj/Debug/net6.0/DotNetCoreMyTeApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetCoreMyTeApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1635bb16ec4b41e7e0efee06688cf55b723347b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02084367d2ce3b121d758a074aeb3ab544edfd5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetCoreMyTeApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetCoreMyTeApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

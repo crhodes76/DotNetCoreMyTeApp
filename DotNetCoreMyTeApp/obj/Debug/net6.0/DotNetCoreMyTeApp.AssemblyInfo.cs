e3e6f542cffbbc82c75cf3eb771e2b7f338ca75b
Charles Rhodes <crhodes76@users.noreply.github.com>
2025-03-24 16:50:57 -0400

    ran build

--- DotNetCoreMyTeApp/obj/Debug/net6.0/DotNetCoreMyTeApp.AssemblyInfo.cs
+++ DotNetCoreMyTeApp/obj/Debug/net6.0/DotNetCoreMyTeApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetCoreMyTeApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35aac52d80ce44134e7ee7ba11dae1633e6c869c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0a51d4c678e27bd9f85bccacebda8381cda25da")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetCoreMyTeApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetCoreMyTeApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
